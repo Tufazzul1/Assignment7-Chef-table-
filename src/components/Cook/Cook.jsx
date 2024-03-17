4e97422a45eb1027ab30ecbe9022e2db15d2a5d4
Tufazzul1 <tufajjul2017@gmail.com>
2024-03-17 16:24:51 +0545

    live link cheak

--- src/components/Cook/Cook.jsx
+++ src/components/Cook/Cook.jsx
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
 
 const Cook = ({ cook }) => {
-    const { recipe_name, preparing_time, calories } = cook
+    const { recipe_name, preparing_time, calories,recipe_id } = cook
     return (
      
             <div className='p-4 m-4 bg-gray-100 rounded-lg flex gap-2 justify-between'>
+                <h3>{recipe_id}. </h3>
                 <h3>{recipe_name}</h3>
                 <h4>{preparing_time} minutes</h4>
                 <p>{calories} calories</p>
