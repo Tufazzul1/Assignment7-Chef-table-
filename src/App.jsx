032d24de87fb328d990c514ed8400c5b35e2083a
Tufazzul1 <tufajjul2017@gmail.com>
2024-03-18 01:22:37 +0545

    added the toast

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,28 @@ import Header from './components/Header/Header'
 import RecipeHeader from './components/Recipeheader/RecipeHeader'
 import Recipes from './components/Recipes/Recipes'
 import Cooks from './components/Cooks/Cooks'
-
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
 
   const [cooks, setCooks] = useState([])
 
-  const handleAddToCook = recipe =>{
-    const newRecipeList = [...cooks, recipe]
-    setCooks(newRecipeList)
-  }
+  const handleAddToCook = recipe => {
+    if (cooks.find(cook => cook.recipe_id === recipe.recipe_id)) {
+      toast.error('Recipe already added to cooks');
+    } else {
+      const newRecipeList = [...cooks, recipe];
+      setCooks(newRecipeList);
+      toast.success('Recipe added to cooks');
+    }
+  };
+
 
   const [currentCooks, setCurrentCooks] = useState([])
 
   const handleCurrentCook = currCook => {
-      const newCurrentCook = [...currentCooks, currCook]
-      setCurrentCooks(newCurrentCook)
+    const newCurrentCook = [...currentCooks, currCook]
+    setCurrentCooks(newCurrentCook)
   }
 
   return (
@@ -30,9 +37,10 @@ function App() {
       <RecipeHeader></RecipeHeader>
       <div className='lg:flex container mx-auto gap-3'>
         <Recipes handleAddToCook={handleAddToCook}></Recipes>
-        <Cooks handleCurrentCook={handleCurrentCook} cooks={cooks} currentCooks={currentCooks}></Cooks>
+        <Cooks handleCurrentCook={handleCurrentCook} cooks={cooks} currentCooks={currentCooks}>
+        </Cooks>
       </div>
-
+      <ToastContainer />
     </>
   )
 }
